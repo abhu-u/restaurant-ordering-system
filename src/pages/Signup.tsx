6690cc8dd54adaff3d3038caf6efe7b6eb85c565
abhu-u <itsudit.py@gmail.com>
2025-10-09 18:54:00 +0530

    Signup.tsx

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -47,7 +47,7 @@ const Signup = () => {
 
     try {
       setLoading(true);
-      const response = await fetch("http://localhost:5000/api/auth/register", {
+      const response = await fetch("https://restaurant-ordering-system-9jcp.onrender.com/api/auth/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
